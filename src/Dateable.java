d152b5e128117fd95e08baa6a1d2e3287d16a4c9
valerieeho <valerieh2027@student.cis.edu.hk>
2025-01-24 13:05:13 +0800

    done

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,8 +1,23 @@
+import java.util.Scanner;
+
 public class Dateable {
 
   public static void main(String[] args) {
-      // Write your code below, inside the main method.
-    
+      Scanner scanner = new Scanner(System.in);
+      System.out.println("What is your age?");
+      int age = Integer.parseInt(scanner.nextLine());
+      while (age < 0) {
+          try {
+              age = Integer.parseInt(scanner.nextLine());
+          }
+          catch (NumberFormatException e) {
+              System.out.println("Please enter a valid number!");
+          }
+      }
+      int youngestAge = ( 7 + ( age / 2 ) );
+      System.out.println(age + "-year olds should date somebody who is at least " + youngestAge + " years old");
+
+
   }
   
 }
